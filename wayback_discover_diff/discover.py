3e0f2a7c0d79ea1bdd51b9bc1874cc44c21cacb6
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-06-28 22:38:07 +0300

    Add error handling

--- wayback_discover_diff/discover.py
+++ wayback_discover_diff/discover.py
@@ -1,3 +1,4 @@
+import builtins
 from flask import (flash, jsonify)
 import urllib3
 import json
@@ -17,11 +18,11 @@ class Discover(object):
             error = 'Timestamp is required.'
         else:
             http = urllib3.PoolManager()
-            r = http.request('GET', 'https://web.archive.org/web/'+timestamp+'/'+url)
+            r = http.request('GET', 'https://web.archive.org/web/' + timestamp + '/' + url)
             if r.status == 200:
                 simhash_result = {}
                 simhash_result['simhash'] = Simhash(r.data.decode('utf-8')).value
-                simhash_result =json.dumps(simhash_result)
+                simhash_result = json.dumps(simhash_result)
             else:
                 simhash_result = 'None'
         flash(error)
@@ -38,18 +39,17 @@ class Discover(object):
             error = 'Year is required.'
         else:
             http = urllib3.PoolManager()
-            print('https://web.archive.org/cdx/search/cdx?url='+url+
-                  '&from='+year+'&to='+year+'&fl=timestamp&output=json&output=json&limit=3')
-            r = http.request('GET', 'https://web.archive.org/cdx/search/cdx?url='+url+'}&'
-                                    'from='+year+'&to='+year+'&fl=timestamp&output=json&output=json&limit=3')
-            print(r.data.decode('utf-8'))
-            snapshots = json.loads(r.data.decode('utf-8'))
-            snapshots.pop(0)
-            simhashes = []
-            for snapshot in snapshots:
-                print(f'https://web.archive.org/web/{snapshot[0]}/{url}')
-                r = http.request('GET', 'https://web.archive.org/web/'+snapshot[0]+'/'+url)
-                simhashes.append(Simhash(r.data.decode('utf-8')).value)
+            r = http.request('GET', 'https://web.archive.org/cdx/search/cdx?url=' + url + '}&'
+                                        'from=' + year + '&to=' + year + '&fl=timestamp&output=json&output=json&limit=3')
+            try:
+                snapshots = json.loads(r.data.decode('utf-8'))
+                snapshots.pop(0)
+                simhashes = []
+                for snapshot in snapshots:
+                    r = http.request('GET', 'https://web.archive.org/web/' + snapshot[0] + '/' + url)
+                    simhashes.append(Simhash(r.data.decode('utf-8')).value)
+            except (json.decoder.JSONDecodeError, builtins.IndexError) as e:
+                return json.dumps({'Message': 'Failed to fetch snapshots, please try again.'})
         flash(error)
 
         return jsonify(simhashes)

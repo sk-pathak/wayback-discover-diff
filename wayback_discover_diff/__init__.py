57c95dcda2a09a8c36622fb05325b7e90aea0946
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-06-28 22:05:55 +0300

    Move logic to discover.py

--- wayback_discover_diff/__init__.py
+++ wayback_discover_diff/__init__.py
@@ -1,7 +1,7 @@
 import os
-from flask import (Flask, flash, jsonify, request)
-import urllib3
-from simhash import Simhash
+from flask import (Flask, request)
+from .discover import Discover
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -25,49 +25,10 @@ def create_app(test_config=None):
 
     @app.route('/simhash')
     def simhash():
-        url = request.args.get('url')
-        timestamp = request.args.get('timestamp')
-        error = None
-        if not url:
-            error = 'URL is required.'
-        elif not timestamp:
-            error = 'Timestamp is required.'
-        else:
-            http = urllib3.PoolManager()
-            r = http.request('GET', 'https://web.archive.org/web/'+timestamp+'/'+url)
-            if r.status == 200:
-                simhash_result = {}
-                simhash_result['simhash'] = Simhash(r.data.decode('utf-8')).value
-            else:
-                simhash_result = 'None'
-        flash(error)
-
-        return jsonify(simhash_result)
+        return Discover.simhash(request)
 
     @app.route('/request')
-    def requestURL():
-        url = request.args.get('url')
-        year = request.args.get('year')
-        error = None
-        if not url:
-            error = 'URL is required.'
-        elif not year:
-            error = 'Year is required.'
-        else:
-            http = urllib3.PoolManager()
-            print('https://web.archive.org/cdx/search/cdx?url='+url+
-                  '&from='+year+'&to='+year+'&fl=timestamp&output=json&output=json&limit=3')
-            r = http.request('GET', 'https://web.archive.org/cdx/search/cdx?url='+url+'}&'
-                                                                                      'from='+year+'&to='+year+'&fl=timestamp&output=json&output=json&limit=3')
-            snapshots = jsonify(r.data.decode('utf-8'))
-            snapshots.pop(0)
-            simhashes = []
-            for snapshot in snapshots:
-                print(f'https://web.archive.org/web/{snapshot[0]}/{url}')
-                r = http.request('GET', 'https://web.archive.org/web/'+snapshot[0]+'/'+url)
-                simhashes.append(Simhash(r.data.decode('utf-8')).value)
-        flash(error)
-
-        return jsonify(simhashes)
+    def request_url():
+        return Discover.request_url(request)
 
     return app

3d5b55c1775d414125aba1824f2ad75eb2725a96
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-06-29 12:14:57 +0300

    Fetch simhash from redis

--- wayback_discover_diff/discover.py
+++ wayback_discover_diff/discover.py
@@ -4,6 +4,7 @@ import json
 from simhash import Simhash
 import redis
 
+
 class Discover(object):
 
     @staticmethod
@@ -16,15 +17,14 @@ class Discover(object):
         elif not timestamp:
             error = 'Timestamp is required.'
         else:
-            http = urllib3.PoolManager()
-            r = http.request('GET', 'https://web.archive.org/web/' + timestamp + '/' + url)
-            if r.status == 200:
-                simhash_result = {}
-                simhash_result['simhash'] = Simhash(r.data.decode('utf-8')).value
-                simhash_result = json.dumps(simhash_result)
+            redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
+            results = redis_db.get(url + timestamp)
+            if results is not None:
+                simhash_result = {'simhash': results}
             else:
-                simhash_result = 'None'
+                simhash_result = {'simhash': 'None'}
         flash(error)
+        simhash_result = json.dumps(simhash_result)
         return simhash_result
 
     @staticmethod
@@ -39,20 +39,18 @@ class Discover(object):
         else:
             http = urllib3.PoolManager()
             r = http.request('GET', 'https://web.archive.org/cdx/search/cdx?url=' + url + '&'
-                                        'from=' + year + '&to=' + year + '&fl=timestamp&output=json&output=json&limit=3')
+                                                                                          'from=' + year + '&to=' + year + '&fl=timestamp&output=json&output=json&limit=3')
             try:
                 snapshots = json.loads(r.data.decode('utf-8'))
                 if len(snapshots) == 0:
                     raise ValueError
                 snapshots.pop(0)
                 simhashes = []
-                # create a connection to the localhost Redis server instance, by
-                # default it runs on port 6379
                 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
                 for snapshot in snapshots:
                     r = http.request('GET', 'https://web.archive.org/web/' + snapshot[0] + '/' + url)
                     temp_simhash = Simhash(r.data.decode('utf-8')).value
-                    redis_db.set(url+snapshot[0], temp_simhash)
+                    redis_db.set(url + snapshot[0], temp_simhash)
                     simhashes.append(temp_simhash)
             except (ValueError) as e:
                 return json.dumps({'Message': 'Failed to fetch snapshots, please try again.'})

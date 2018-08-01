de63fc164b689d76b4bd5e329ac7a99e9b7029da
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-08-02 02:12:53 +0300

    Fix most pylint issues

--- wayback_discover_diff/__init__.py
+++ wayback_discover_diff/__init__.py
@@ -1,64 +1,67 @@
 import os
-from flask import (Flask, request, jsonify, json)
-import yaml
-from wayback_discover_diff.discover import Discover
+
 from celery import (Celery, states)
 from celery.result import AsyncResult
+from flask import (Flask, request, jsonify, json)
+import yaml
+
+from wayback_discover_diff.discover import Discover
 
 # create and configure the app
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
+APP = Flask(__name__, instance_relative_config=True)
+APP.config.from_mapping(
     SECRET_KEY='dev',
 )
-app.config.update(CELERYD_HIJACK_ROOT_LOGGER=False)
+APP.config.update(CELERYD_HIJACK_ROOT_LOGGER=False)
 with open(os.environ['WAYBACK_DISCOVER_DIFF_CONF'], 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+    CFG = yaml.load(ymlfile)
 
-app.config.update(
-    CELERY_BROKER_URL='redis://'+str(cfg['redis']['host'])+':'+str(cfg['redis']['port']),
-    CELERY_RESULT_BACKEND='redis://'+str(cfg['redis']['host'])+':'+str(cfg['redis']['port'])
+APP.config.update(
+    CELERY_BROKER_URL='redis://' + str(CFG['redis']['host']) + ':' + str(CFG['redis']['port']),
+    CELERY_RESULT_BACKEND='redis://' + str(CFG['redis']['host']) + ':' + str(CFG['redis']['port'])
 )
 
-app.config.from_pyfile('config.py', silent=True)
+APP.config.from_pyfile('config.py', silent=True)
 
 # NOTE it is a known issue that with the following we instantiate 2 Discovery
 # objects and create 2 Redis connections. There is certainly a way to have
 # only one. I couldn't find a way to run `app.discovery.simhash` in another way.
-app.discover = Discover(cfg)
+APP.discover = Discover(CFG)
 
 # ensure  the instance folder exists
 try:
-    os.makedirs(app.instance_path)
+    os.makedirs(APP.instance_path)
 except OSError:
     pass
 
 # Initialize Celery and register Discover task.
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-celery.register_task(Discover(cfg))
+CELERY = Celery(APP.name, broker=APP.config['CELERY_BROKER_URL'])
+CELERY.conf.update(APP.config)
+CELERY.register_task(Discover(CFG))
 
 
-@app.route('/simhash')
+@APP.route('/simhash')
 def simhash():
     url = request.args.get('url')
     timestamp = request.args.get('timestamp')
     if not timestamp:
         year = request.args.get('year')
-        return app.discover.year_simhash(url, year)
-    return app.discover.timestamp_simhash(url, timestamp)
+        return APP.discover.year_simhash(url, year)
+    return APP.discover.timestamp_simhash(url, timestamp)
 
 
-@app.route('/calculate-simhash')
+@APP.route('/calculate-simhash')
 def request_url():
     url = request.args.get('url')
     year = request.args.get('year')
-    return jsonify({'status': 'started', 'job_id': str(celery.tasks['Discover'].apply_async(args=[url, year]))})
+    return jsonify({'status': 'started',
+                    'job_id': str(CELERY.tasks['Discover'].apply_async(args=[url, year]))})
 
 
-@app.route('/job')
+@APP.route('/job')
 def job_status():
     job_id = request.args.get('job_id')
-    task = AsyncResult(job_id, app=celery)
+    task = AsyncResult(job_id, app=CELERY)
     if task.state == states.PENDING:
         # job did not finish yet
         response = {
@@ -71,7 +74,7 @@ def job_status():
         if task_info.get('status', 0) == 'error':
             # something went wrong in the background job
             response = {
-                'info': task_info.get('info',1),
+                'info': task_info.get('info', 1),
                 'job_id': task.id,
                 'status': task_info.get('status', 0)
             }
@@ -85,4 +88,4 @@ def job_status():
 
 
 if __name__ == '__main__':
-    app.run()
+    APP.run()
--- wayback_discover_diff/discover.py
+++ wayback_discover_diff/discover.py
@@ -1,17 +1,16 @@
 import json
-from simhash import Simhash
-import redis
-import urllib3
-import datetime
-from celery import Task
-import logging
-from celery.utils.log import get_task_logger
 import concurrent.futures
-from celery.contrib import rdb
-import xxhash
+import logging
+import datetime
 import cProfile
-from bs4 import BeautifulSoup
 from itertools import groupby
+from bs4 import BeautifulSoup
+import xxhash
+from celery.utils.log import get_task_logger
+from celery import Task
+import urllib3
+import redis
+from simhash import Simhash
 
 # https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
 urllib3.disable_warnings()
@@ -35,65 +34,64 @@ class Discover(Task):
         self.snapshots_number = cfg['snapshots']['number_per_year']
         self.redis_db = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
 
-        #Initialize logger
+        # Initialize logger
         self._log = logging.getLogger(__name__)
         logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s',
                             filename=self.logfile, level=logging.INFO)
-        #Initialize Task logger
+        # Initialize Task logger
         self._task_log = get_task_logger(__name__)
 
     def timestamp_simhash(self, url, timestamp):
         if not url:
             self._log.error('did not give url parameter')
             return json.dumps({'error': 'URL is required.'})
-        elif not timestamp:
+        if not timestamp:
             self._log.error('did not give timestamp parameter')
             return json.dumps({'error': 'Timestamp is required.'})
-        else:
-            self._log.info('requesting redis db entry for %s %s', url, timestamp)
-            results = self.redis_db.hget(url, timestamp)
-            if results:
-                results = results.decode('utf-8')
-                self._log.info('found entry %s', results)
-                return json.dumps({'simhash': results})
-            else:
-                self._log.info('entry not found')
-                return json.dumps({'simhash': 'None'})
+        self._log.info('requesting redis db entry for %s %s', url, timestamp)
+        results = self.redis_db.hget(url, timestamp)
+        if results:
+            results = results.decode('utf-8')
+            self._log.info('found entry %s', results)
+            return json.dumps({'simhash': results})
+        self._log.info('entry not found')
+        return json.dumps({'simhash': 'None'})
 
     def year_simhash(self, url, year):
         if not url:
             self._log.error('did not give url parameter')
             return json.dumps({'error': 'URL is required.'})
-        elif not year:
+        if not year:
             self._log.error('did not give year parameter')
             return json.dumps({'error': 'Year is required.'})
-        else:
-            self._log.info('requesting redis db entry for %s %s', url, year)
-            results = self.redis_db.hkeys(url)
-            if results:
-                available_simhashes = []
-                for timestamp in results:
-                    timestamp = timestamp.decode('UTF-8')
-                    timestamp_year = timestamp[:4]
-                    if timestamp_year == str(year):
-                        simhash = self.redis_db.hget(url, timestamp).decode('utf-8')
-                        available_simhashes.append({'timestamp': timestamp, 'simhash': simhash})
-                    self._log.info('found entry %s', timestamp)
-                return json.dumps(available_simhashes)
-            else:
-                self._log.info('No simhases for this URL and Year')
-                return json.dumps({'simhash': 'None'})
+        self._log.info('requesting redis db entry for %s %s', url, year)
+        results = self.redis_db.hkeys(url)
+        if results:
+            available_simhashes = []
+            for timestamp in results:
+                timestamp = timestamp.decode('UTF-8')
+                timestamp_year = timestamp[:4]
+                if timestamp_year == str(year):
+                    simhash = self.redis_db.hget(url, timestamp).decode('utf-8')
+                    available_simhashes.append({'timestamp': timestamp, 'simhash': simhash})
+                self._log.info('found entry %s', timestamp)
+            return json.dumps(available_simhashes)
+        self._log.info('No simhases for this URL and Year')
+        return json.dumps({'simhash': 'None'})
 
     def download_snapshot(self, snapshot, url, i, total):
         self._task_log.info('fetching snapshot %d out of %d', i, total)
         # rdb.set_trace()
-        # self.update_state(task_id= self.request.id, state='PENDING', meta={'info': ' captures have been processed'})
-        r = self.http.request('GET', 'http://web.archive.org/web/' + snapshot[0] + '/' + url)
+        # self.update_state(task_id= self.request.id, state='PENDING',
+        # meta={'info': ' captures have been processed'})
+        response = self.http.request('GET', 'http://web.archive.org/web/' + snapshot[0] + '/' + url)
         self._task_log.info('calculating simhash for snapshot %d out of %d', i, total)
-        return r
+        return response
 
-    def start_simhash_import(self, snapshot, url, i, total):
-        cProfile.runctx('self.get_calc_save(snapshot, url, i, total)', globals=globals(), locals=locals(), filename='profile.prof')
+    def start_simhash_import(self, snapshot,
+                             url, i, total):
+        cProfile.runctx('self.get_calc_save(snapshot, url, i, total)',
+                        globals=globals(), locals=locals(), filename='profile.prof')
 
     def get_calc_save(self, snapshot, url, i, total):
         data = self.download_snapshot(snapshot, url, i, total)
@@ -101,12 +99,12 @@ class Discover(Task):
         simhash = self.calculate_simhash(data)
         self.save_to_redis(url, snapshot, simhash, total)
 
-    def calc_features(self, r):
-        soup = BeautifulSoup(r.data.decode('utf-8', 'ignore'))
+    def calc_features(self, response):
+        soup = BeautifulSoup(response.data.decode('utf-8', 'ignore'))
 
         # kill all script and style elements
         for script in soup(["script", "style"]):
-            script.extract()    # rip it out
+            script.extract()  # rip it out
 
         # get text
         text = soup.get_text()
@@ -121,7 +119,7 @@ class Discover(Task):
 
         text = text.split()
 
-        text = {k:sum(1 for _ in g) for k, g in groupby(sorted(text))}
+        text = {k: sum(1 for _ in g) for k, g in groupby(sorted(text))}
 
         return text
 
@@ -136,53 +134,59 @@ class Discover(Task):
         self._task_log.info('calculate simhash started')
         if not url:
             self._task_log.error('did not give url parameter')
-            result = {'status':'error', 'info': 'URL is required.'}
+            result = {'status': 'error', 'info': 'URL is required.'}
         elif not year:
             self._task_log.error('did not give year parameter')
-            result = {'status':'error', 'info': 'Year is required.'}
+            result = {'status': 'error', 'info': 'Year is required.'}
         else:
             try:
                 self._task_log.info('fetching timestamps of %s for year %s', url, year)
                 self.update_state(state='PENDING',
-                                  meta={'info': 'Fetching timestamps of ' + url + ' for year ' + year})
-                wayback_url = 'http://web.archive.org/cdx/search/cdx?url=' + url + '&' + 'from=' + year + '&to=' + year + '&fl=timestamp&output=json'
+                                  meta={'info': 'Fetching timestamps of '
+                                                + url + ' for year ' + year})
+                wayback_url = 'http://web.archive.org/cdx/search/cdx?url=' + url + \
+                              '&' + 'from=' + year + '&to=' + year + '&fl=timestamp&output=json'
                 if self.snapshots_number != -1:
                     wayback_url += '&limit=' + str(self.snapshots_number)
-                r = self.http.request('GET', wayback_url)
+                response = self.http.request('GET', wayback_url)
                 self._task_log.info('finished fetching timestamps of %s for year %s', url, year)
-                snapshots = json.loads(r.data.decode('utf-8'))
+                snapshots = json.loads(response.data.decode('utf-8'))
 
-                if len(snapshots) == 0:
+                if snapshots:
                     self._task_log.error('no snapshots found for this year and url combination')
-                    result = {'status':'error', 'info': 'no snapshots found for this year and url combination'}
+                    result = {'status': 'error',
+                              'info': 'no snapshots found for this year and url combination'}
                     return json.dumps(result, sort_keys=True)
                 snapshots.pop(0)
                 total = len(snapshots)
-                with concurrent.futures.ThreadPoolExecutor(max_workers=self.thread_number) as executor:
+                with concurrent.futures.ThreadPoolExecutor(max_workers=
+                                                           self.thread_number) as executor:
                     # Start the load operations and mark each future with its URL
-                    #TODO: Fix snapshot number
-                    future_to_url = {executor.submit(self.start_simhash_import, snapshot, url, 1, total): snapshot for snapshot in snapshots}
+                    # TODO: Fix snapshot number
+                    future_to_url = {executor.submit(self.start_simhash_import,
+                                                     snapshot, url, 1, total):
+                                         snapshot for snapshot in snapshots}
                     for future in concurrent.futures.as_completed(future_to_url):
                         try:
                             future.result()
                         except Exception as exc:
                             self._task_log.error(exc)
-            except Exception as e:
-                self._task_log.error(e.args[0])
-                result = {'status':'error', 'info': e.args[0]}
+            except Exception as exc:
+                self._task_log.error(exc.args[0])
+                result = {'status': 'error', 'info': exc.args[0]}
                 return json.dumps(result, sort_keys=True)
             time_ended = datetime.datetime.now()
             result = {'duration': str((time_ended - time_started).seconds)}
-            self._task_log.info('calculate simhash ended with duration: %d', (time_ended - time_started).seconds)
+            self._task_log.info('calculate simhash ended with duration: %d',
+                                (time_ended - time_started).seconds)
             return json.dumps(result, sort_keys=True)
         return json.dumps(result, sort_keys=True)
 
     def save_to_redis(self, url, snapshot, data, total):
-        #TODO: Fix snapshot number
+        # TODO: Fix snapshot number
         self._task_log.info('saving to redis simhash for snapshot %d out of %d', 1, total)
         self.redis_db.hset(url, snapshot[0], data)
 
 
 def hash_function(x):
     return int(xxhash.xxh64(x).hexdigest(), 16)
-

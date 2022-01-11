744261466b820232723b6b066951dd151cf277be
Vangelis Banos <vangelis@archive.org>
2022-01-11 19:10:09 +0000

    Add hiredis for better performance

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'Flask>=2.0.2',
-        'simhash',
+        'simhash>=2.1.1',
         'urllib3>=1.25.6',
         'PyYAML>=5.3.1,<6',
         # required for Celery
@@ -23,6 +23,7 @@ setup(
         'kombu>=5.2.3,<6.0',
         'redis==4.1.0',
 
+        'hiredis',
         'flask-cors',
         'selectolax',
         'statsd',
